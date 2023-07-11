ad40e13ac6ea310280a5e96f80fc3240137e18b5
Thiago Alves <thiagoluizmachadoalves@gmail.com>
2023-07-11 13:43:14 -0300

    Commit organizacional

--- api/routeParams.js
+++
@@ -1,9 +0,0 @@
-// Pegando parâmetros pela rota
-const express = require('express');
-const app = express();
-app.listen('3000');
-
-// ('/:nome') Isso define que o que eu escrever após a barra irá se tornar uma variável
-app.route('/').get((req, res) => res.send("Oi."));
-app.route('/:variavel').get((req, res) => res.send(req.params.variavel));
-app.route('/identidade/:nome').get((req, res) => res.send(req.params.nome));
--- javascript/biblioteca_rocketseat/js_antes_do_react/nullish_coalescing_operator.js
+++
@@ -1,4 +0,0 @@
-// Nullish Coalescing Operator
-// Retorna "Não informado" caso o valor da variável idade seja nulo
-const idade = 0;
-console.log("Sua idade é: " + (idade ?? "Não informado"));
